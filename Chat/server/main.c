24176c273a688a37cdb3a905088be03b0f6d8c43
Guilherme Rezende Alles <guilherme.r.alles@gmail.com>
2016-05-05 15:13:18 -0300

    .

--- Chat/server/main.c
+++ Chat/server/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h> // struct sockaddr_in
 
 int main (int argc, char **argv) {
-	int socket;
+	int connection_socket, message_socket;
+	
+	// Try to open connection socket
+	connection_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (connection_socket == -1) {
+		fpritnf(stderr, "Error creating connection socket.\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	// Fill the struct sockaddr_in variable
 }
-
-/* Melhor programa */
