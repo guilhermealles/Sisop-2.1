3fd70f472e938bb21f1f8845c376ff9f62da773b
Guilherme <guilhermealles@users.noreply.github.com>
2016-05-13 16:36:15 -0300

    mudanças client

--- Chat/interface.h
+++ Chat/interface.h
@@ -4,6 +4,13 @@
 #define MAX_CLIENTS_PER_ROOM 30
 #define SERVER_PORT 3010
 
+// List of all possible "tags"
+#define LIST_ROOMS 1
+#define JOIN_ROOM 2
+#define MESSAGE_TO_ROOM 3
+#define LEAVE_ROOM 4
+#define CHANGE_NICK 5
+
 typedef struct client {
     unsigned int    clientId;
     char*           name;
