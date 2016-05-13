67da5a6f2f02798fa94d9b93b959532ea4be1fc1
Amanda T <amandarietht@gmail.com>
2016-05-13 00:25:53 -0300

    Thread para receber mensagens

--- Chat/client/client.c
+++ Chat/client/client.c
@@ -4,6 +4,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h> // struct sockaddr_in
 #include "../interface.h"
+#include <pthread.h>
 
 #define	SOCKET	int
 #define INVALID_SOCKET  ((SOCKET)~0)
@@ -14,22 +15,52 @@
 
 void connectToServer();
 void setNick();
+void socketReceiver();
 
 SOCKET s;
 struct sockaddr_in  s_cli, s_serv;
 int porta_cli;
 char nick[32];
+char receiveBuffer[BUFF];
+
 
 
 int main (int argc, char **argv){
+	pthread_t thread;
 
 	connectToServer();
+
+	if (pthread_create(&thread, NULL, (void *)socketReceiver, NULL) != 0) {
+		fprintf(stderr, "Error when creating a thread.\n");
+		exit(EXIT_FAILURE);
+	} 
+
 	printf("Welcome to Earth chat!! \n");
 	setNick();
 
+
 	// acoes clientes
+	
+	pthread_join(thread, NULL);
+}
+void socketReceiver(){
+	int confirm;
+	while(1){
+		bzero(receiveBuffer, 1024);
+		confirm = read(s, receiveBuffer, 1024);
+
+		if (confirm < 0) {
+		  perror("ERROR reading from socket");
+		  exit(1);
+		}
+
+		printf("buffer: %s\n",receiveBuffer);
+	}
+   return;
+
 
 }
+
 
 void setNick(){
 
@@ -41,47 +72,29 @@ void setNick(){
 	printf("Please enter your nick name: \n");
 	fgets(nick, 32, stdin);	
 
+	// seta tamanho dos pacotes
 	nick_package = malloc(strlen(nick) + 1 + 4); // tag + int
-	package_length = strlen(nick_package);
+	package_length = strlen(nick) + 5;
+	printf("tamanho pacote: %d\n", package_length);
+
+	// concatena informacoes do pacote
 	sprintf(nick_package,"%s%s%d",nick_tag, nick_tag, package_length);
 	
-	confirm = write(s, nick_package, sizeof(nick_package));  //(const char *)&
+	// envia para o servidor
+	confirm = write(s, nick_package, sizeof(nick_package));
+
 	if (confirm < 0){
 		printf("Erro na transmissão\n");
 		close(s);
 		return;
 	}
-	   
-   /* TODO: while até receber tudo
-   // ler primeiro byte para ver o tamanho
-   bzero(buffer, 1024);
-   confirm = read(sockfd, buffer, 1024);
-   
-   if (confirm < 0) {
-      perror("ERROR reading from socket");
-      exit(1);
-   }
-	
-   printf("%s\n",buffer);
-   return 0;
-*/
-
 
 }
+
 void connectToServer(){	
 
 	if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET){
 		printf("Erro iniciando socket\n");
-		return;
-	}
-
-	s_cli.sin_family = AF_INET;
-	s_cli.sin_addr.s_addr = htonl(INADDR_ANY);
-	s_cli.sin_port = htons(PORTA_CLI);
-
-	if ((bind(s, (struct sockaddr *)&s_cli, sizeof(s_cli))) != 0){
-		printf("Erro no bind\n");
-		close(s);
 		return;
 	}
 
